eef6231baac9b09c4f326cc6d01556f12d74c8de
Abdulhakeem Abdulazeez <ayoazeez26@gmail.com>
2023-08-21 11:06:34 +0100

    feat: added gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,34 +4,48 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
     head: {
-      title: 'O.C. Management Consulting',
-      link: [
-        { rel: "icon", type: "image/png", href: "/favicon.ico"}
-      ],
+      title: "O.C. Management Consulting",
+      link: [{ rel: "icon", type: "image/png", href: "/favicon.ico" }],
       meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: 'Leading management consulting company specializing in Tender, Grant, and Bid writing and management as well as developing quality assurance frameworks and enabling successful license applications.' }
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          name: "description",
+          content:
+            "Leading management consulting company specializing in Tender, Grant, and Bid writing and management as well as developing quality assurance frameworks and enabling successful license applications.",
+        },
       ],
       script: [
         {
-          hid: 'tawk.to',
-          src: 'https://embed.tawk.to/64b65abccc26a871b0291a35/1h5k54spo',
+          hid: "tawk.to",
+          src: "https://embed.tawk.to/64b65abccc26a871b0291a35/1h5k54spo",
           async: true,
           defer: true,
-        }
-      ]
-    }
+        },
+      ],
+    },
   },
-  css: ['~/assets/main.scss', '~/assets/fonts/fonts.css'],
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxtjs/google-fonts', 'nuxt-swiper', '@pinia/nuxt'],
+  css: ["~/assets/main.scss", "~/assets/fonts/fonts.css"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-icon",
+    "@nuxtjs/google-fonts",
+    "nuxt-swiper",
+    "@pinia/nuxt",
+    "nuxt-gtag",
+  ],
+
+  gtag: {
+    id: "G-KV47Z7SGCC",
+  },
+
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@use "@/assets/_colors.scss" as *;'
-        }
-      }
-    }
-  }
-})
+          additionalData: '@use "@/assets/_colors.scss" as *;',
+        },
+      },
+    },
+  },
+});
