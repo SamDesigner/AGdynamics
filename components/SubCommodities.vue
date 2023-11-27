177cad86c009ab56036da2d0ed0a41c3341979dd
SamDesigner <olabodesamuel08@gmail.com>
2023-11-27 16:56:16 +0100

    Resolution Update

--- components/SubCommodities.vue
+++ components/SubCommodities.vue
@@ -4,7 +4,7 @@
             <div class="w-[704px] bg-faintYellow  rounded-tl-[83px] rounded-bl-[83px] rounded-br-[83px]">
                 <div class="px-[64px] py-[64px]">         
                     <div class="w-[425px] flex flex-col gap-[20px]">
-                        <h1 class="text-maroon text-[25px] font-[600] ">Export Fulfillment Services:</h1>
+                        <h1 class="text-maroon text-[25px] font-[500] ">Export Fulfillment Services:</h1>
                         <p class="text-customGray-4 text-[14px] leading-[26px]">Explanation of how export fulfillment services open up new markets and buyers.</p>
                         <NuxtLink to="#" class="flex items-center text-primaryGreen text-[14px] gap-[5px] "><span class="hover:underline">Learn more</span> <span><img src="/svg/arrow-right.svg" /></span></NuxtLink>
                     </div>
@@ -18,7 +18,7 @@
             <div class="w-[581px] bg-faintYellow  rounded-tl-[83px] rounded-bl-[83px] rounded-br-[83px]">
                 <div class="px-[64px] py-[64px]">         
                     <div class="w-[425px] flex flex-col gap-[20px]">
-                        <h1 class="text-maroon text-[25px] font-[600] ">Mineral Sector Promotion:</h1>
+                        <h1 class="text-maroon text-[25px] font-[500] ">Mineral Sector Promotion:</h1>
                         <p class="text-customGray-4 text-[14px] leading-[26px]">Details on promoting the solid mineral sector to investors.</p>
                         <NuxtLink to="#" class="flex items-center text-primaryGreen text-[14px] gap-[5px] "><span class="hover:underline">Learn more</span> <span><img src="/svg/arrow-right.svg" /></span></NuxtLink>
                     </div>
@@ -50,7 +50,7 @@
             <div class="w-[581px] bg-faintYellow  rounded-tl-[83px] rounded-bl-[83px] rounded-br-[83px]">
                 <div class="px-[64px] py-[64px]">         
                     <div class="w-[425px] flex flex-col gap-[20px]">
-                        <h1 class="text-maroon text-[25px] font-[600] ">Guarantee offtake</h1>
+                        <h1 class="text-maroon text-[25px] font-[500] ">Guarantee offtake</h1>
                         <p class="text-customGray-4 text-[14px] leading-[26px]">Ensuring the offtake of all commodities marketed through the platform.</p>
                         <NuxtLink to="#" class="flex items-center text-primaryGreen text-[14px] gap-[5px] "><span class="hover:underline">Learn more</span> <span><img src="/svg/arrow-right.svg" /></span></NuxtLink>
                     </div>
@@ -65,7 +65,7 @@
             <div class="w-[704px] bg-faintYellow  rounded-tr-[83px] rounded-bl-[83px] rounded-br-[83px]">
                 <div class="px-[64px] py-[64px]">         
                     <div class="w-[425px] flex flex-col gap-[20px]">
-                        <h1 class="text-maroon text-[25px] font-[600] ">Commodity Aggregation Promotion</h1>
+                        <h1 class="text-maroon text-[25px] font-[500] ">Commodity Aggregation Promotion</h1>
                         <p class="text-customGray-4 text-[14px] leading-[26px]">Details on promoting the commodity aggregation service to farmers and miners.</p>
                         <NuxtLink to="#" class="flex items-center text-primaryGreen text-[14px] gap-[5px] "><span class="hover:underline">Learn more</span> <span><img src="/svg/arrow-right.svg" /></span></NuxtLink>
                     </div>
