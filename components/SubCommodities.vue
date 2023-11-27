a387ba844020893e9876ecbaaa965e8b74cf2af1
SamDesigner <olabodesamuel08@gmail.com>
2023-11-27 16:42:52 +0100

    First Round

--- components/SubCommodities.vue
+++ components/SubCommodities.vue
@@ -1,9 +1,80 @@
 <template>
-    <div class="px-[64px]">
-        <div class="flex items-center">
-            <div class="">
-
+    <div class="px-[64px] flex flex-col gap-[25px]">
+        <div class="flex items-center gap-[25px]">
+            <div class="w-[704px] bg-faintYellow  rounded-tl-[83px] rounded-bl-[83px] rounded-br-[83px]">
+                <div class="px-[64px] py-[64px]">         
+                    <div class="w-[425px] flex flex-col gap-[20px]">
+                        <h1 class="text-maroon text-[25px] font-[600] ">Export Fulfillment Services:</h1>
+                        <p class="text-customGray-4 text-[14px] leading-[26px]">Explanation of how export fulfillment services open up new markets and buyers.</p>
+                        <NuxtLink to="#" class="flex items-center text-primaryGreen text-[14px] gap-[5px] "><span class="hover:underline">Learn more</span> <span><img src="/svg/arrow-right.svg" /></span></NuxtLink>
+                    </div>
+                </div>
+                <div class="flex justify-end">
+                    <img src="/img/malefarmer.png" />
+                </div>
             </div>
+
+
+            <div class="w-[581px] bg-faintYellow  rounded-tl-[83px] rounded-bl-[83px] rounded-br-[83px]">
+                <div class="px-[64px] py-[64px]">         
+                    <div class="w-[425px] flex flex-col gap-[20px]">
+                        <h1 class="text-maroon text-[25px] font-[600] ">Mineral Sector Promotion:</h1>
+                        <p class="text-customGray-4 text-[14px] leading-[26px]">Details on promoting the solid mineral sector to investors.</p>
+                        <NuxtLink to="#" class="flex items-center text-primaryGreen text-[14px] gap-[5px] "><span class="hover:underline">Learn more</span> <span><img src="/svg/arrow-right.svg" /></span></NuxtLink>
+                    </div>
+                </div>
+                <div class="flex justify-end">
+                    <img src="/img/oldschool.png" />
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <div class="flex items-center gap-[25px]">
+
+
+            <div class="w-[581px] bg-faintYellow  rounded-tl-[83px] rounded-bl-[83px] rounded-br-[83px]">
+                <div class="px-[64px] py-[64px]">         
+                    <div class="w-[425px] flex flex-col gap-[20px]">
+                        <h1 class="text-maroon text-[25px] font-[600] ">Guarantee offtake</h1>
+                        <p class="text-customGray-4 text-[14px] leading-[26px]">Ensuring the offtake of all commodities marketed through the platform.</p>
+                        <NuxtLink to="#" class="flex items-center text-primaryGreen text-[14px] gap-[5px] "><span class="hover:underline">Learn more</span> <span><img src="/svg/arrow-right.svg" /></span></NuxtLink>
+                    </div>
+                </div>
+                <div class="flex justify-end">
+                    <img src="/img/export.png" />
+                </div>
+            </div>
+
+
+
+            <div class="w-[704px] bg-faintYellow  rounded-tl-[83px] rounded-bl-[83px] rounded-br-[83px]">
+                <div class="px-[64px] py-[64px]">         
+                    <div class="w-[425px] flex flex-col gap-[20px]">
+                        <h1 class="text-maroon text-[25px] font-[600] ">Commodity Aggregation Promotion</h1>
+                        <p class="text-customGray-4 text-[14px] leading-[26px]">Details on promoting the commodity aggregation service to farmers and miners.</p>
+                        <NuxtLink to="#" class="flex items-center text-primaryGreen text-[14px] gap-[5px] "><span class="hover:underline">Learn more</span> <span><img src="/svg/arrow-right.svg" /></span></NuxtLink>
+                    </div>
+                </div>
+                <div class="flex justify-end">
+                    <img src="/img/trailer.png" />
+                </div>
+            </div>
+
         </div>
     </div>
 </template>
