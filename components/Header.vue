631d65abf183c2b1a131e5c7df45399391bd5032
SamDesigner <olabodesamuel08@gmail.com>
2023-11-29 13:40:02 +0100

    Latest Change

--- components/Header.vue
+++ components/Header.vue
@@ -2,9 +2,9 @@
     <div v-if="isOpen" class="h-screen md:hidden fixed w-[90%] custom_index bg-white">
         ldmlgldgmlm
     </div>
-    <div class="bg-greenBg relative px-[16px] lg:px-[64px] h-[177px] flex items-center w-full">
+    <div class="bg-greenBg px-[16px] lg:px-[64px] h-[177px] flex items-center w-full">
       
-        <nav class="fixed md:sticky bg-white h-[115px] max-w-[1440px] mx-auto flex justify-between items-center pr-[31px] rounded-[20px] w-8/10 md:w-full border-[0.5px] border-primaryGreen ">
+        <nav :class="{'fixed-navbar' : isFixed }" class=" bg-white h-[115px] max-w-[1440px] mx-auto flex justify-between items-center pr-[31px] rounded-[20px] w-8/10 md:w-full border-[0.5px] border-primaryGreen ">
             <NuxtLink to='/'>
                 <img src="/svg/logo.svg" />
             </NuxtLink>
@@ -39,10 +39,33 @@
 
         data(){
             return{
-                isOpen : false
+                isOpen : false,
+                isFixed:false
             }
         },
+        mounted(){
+            const scrollThreshold = 40;
+
+            window.addEventListener('scroll', this.handleScroll);
+
+            this.isFixed = window.scrollY >= scrollThreshold;
+        },
+        beforeDestroy(){
+            window.removeEventListener('scroll', this.handleScroll);
+        },
         methods:{
+            handleScroll(){
+                const scrollY = window.scrollY || window.pageYOffset;
+                const scrollThreshold = 240;
+                this.isFixed = scrollY >= scrollThreshold;
+                
+
+                if (scrollY >= scrollThreshold) {
+                    this.isFixed = true;
+                } else {
+                    this.isFixed = false;
+                }
+            },
             changeIsOpen(){
                 this.isOpen = true
             }
@@ -55,5 +78,23 @@
     }
     .learn_more{
         border: 1px solid #CAC8C8;
+    }
+    .fixed-navbar {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        z-index:3000;
+        background-color: white;
+        border-radius: 0px !important;
+        border:none;
+        border-bottom:2px solid #04A44C;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+
+    /* Add a smooth transition effect for smooth appearance */
+    transition: all 0.3s ease;
+    }
+    .router-link-exact-active{
+        color:#04A44C
     }
 </style>
